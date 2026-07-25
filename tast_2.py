fc52ad80b48ecfec84244ca4c775e20320ba2a21
Myroslava-Hnatiuk <myroslava.hnatiuk@ukr.net>
2026-07-25 06:58:08 +0100

    Implement task 2. Create a function that generates a list of unique random numbers within a specified range. The function should take three parameters: the minimum value, the maximum value, and the quantity of numbers to generate. The function should return a sorted list of unique random numbers. If the input parameters are invalid (e.g., min is greater than max, quantity is greater than the range, or any parameter is not an integer), the function should return None and print an appropriate error message.

---
+++ tast_2.py
@@ -0,0 +1,35 @@
+import random
+
+
+def get_numbers_ticket(min: int, max: int, quantity: int) -> list[int] | None:
+    # Check if all parameters are integers then return None
+    if not all(type(x) is int for x in (min, max, quantity)): 
+        print("Invalid input. Please ensure all parameters are integers.")
+        return None
+    # Check if min is less than 1 then return None
+    if min < 1:
+        print("Invalid input. Please ensure that min is >= 1.")
+        return None
+    # Check if max is greater than 1000 then return None
+    if max > 1000:
+        print("Invalid input. Please ensure that max is <= 1000.")
+        return None
+    # Check if min + quantity is less than or equal to max then return None
+    if max - min + 1 < quantity:
+        print("Invalid input. Please ensure that min + quantity <= max.")
+        return None
+    
+    random_numbers = range(min, max + 1) # Generate a list of random numbers between min and max + 1 to include max
+    selected_amount_of_numbers = random.sample(random_numbers, quantity) # make a random selection of unique numbers from the list of random numbers
+    sorted_numbers = sorted(selected_amount_of_numbers) # sort the selected numbers in ascending order
+    print(sorted_numbers)
+    return sorted_numbers
+
+
+get_numbers_ticket(1, 49, 6) # [1, 4, 5, 12, 19, 36]
+get_numbers_ticket(1, 36, 5) # [6, 8, 14, 16, 17]
+get_numbers_ticket(0, 35, 5) # Invalid input. Please ensure that min is >= 1.
+get_numbers_ticket(1, 1001, 5) # Invalid input. Please ensure that max is <= 1000.
+get_numbers_ticket(1, 10, 15) # Invalid input. Please ensure that min + quantity <= max.
+get_numbers_ticket(1, 10, "five") # Invalid input. Please ensure all parameters are integers.
+get_numbers_ticket(1, 10, 5.5) # Invalid input. Please ensure all parameters are integers.
